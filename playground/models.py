bfedf12ebedf6b72ede87e6b18e6bbf5e70bb57d
kivkiv12345 <kevincarlsen@live.dk>
2021-08-02 17:31:23 +0200

    Database subclassing

--- playground/models.py
+++ playground/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class ParentModel(models.Model):
+    parent_name = models.CharField(max_length=50)
+
+
+class ChildModel(ParentModel):
+    child_name = models.CharField(max_length=50)
